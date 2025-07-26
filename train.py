8a76121be96d421af47efec6e08a96bc5ff59e92
lamedicineniksy <21f1004591@ds.study.iitm.ac.in>
2025-07-26 16:01:16 +0000

    inital commit with Docker file

---
+++ train.py
@@ -0,0 +1,14 @@
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+# Load Iris data
+iris = load_iris()
+X, y = iris.data, iris.target
+
+# Train model
+model = RandomForestClassifier()
+model.fit(X, y)
+
+# Save the trained model to a file
+joblib.dump(model, 'iris_model.pkl')
